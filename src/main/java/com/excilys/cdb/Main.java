70d94bb1524db78891f30c291d791058e504e87b
eherrmann <eherrmann@excilys.com>
2018-04-16 10:57:40 +0200

    Commit initial

---
+++ src/main/java/com/excilys/cdb/Main.java
@@ -0,0 +1,10 @@
+package main.java.com.excilys.cdb;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/excilys/cdb/rtzt.java
@@ -0,0 +1,5 @@
+package main.java.com.excilys.cdb;
+
+public class rtzt {
+
+}
