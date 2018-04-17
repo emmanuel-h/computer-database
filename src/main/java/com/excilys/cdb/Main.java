215cfe4903768999fe9df080bb0c1c15ca121764
eherrmann <eherrmann@excilys.com>
2018-04-17 11:03:30 +0200

    fin daos

--- src/main/java/com/excilys/cdb/Main.java
+++ src/main/java/com/excilys/cdb/Main.java
@@ -1,6 +1,7 @@
 package main.java.com.excilys.cdb;
 
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
 
 import main.java.com.excilys.cdb.daos.CompanyDAO;
@@ -16,7 +17,7 @@ public class Main {
 	public static void main(String[] args) {
 		ComputerDAO computerDAO;
 		try {
-			CompanyDAO companyDAO = (CompanyDAO) DAOFactory.getDAO("company");
+			/*CompanyDAO companyDAO = (CompanyDAO) DAOFactory.getDAO("company");
 			Company company_test = new Company();
 			company_test.setName("test");
 			company_test.setId(43);
@@ -27,15 +28,18 @@ public class Main {
 			List<Company>companies =  companyDAO.findAll();
 			for(Company company: companies) {
 				System.out.println(company);
-			}
+			}*/
 			
-			/*
+			CompanyDAO companyDAO = (CompanyDAO) DAOFactory.getDAO("company");
+			Company company_test = companyDAO.findOneById(1);
 			computerDAO = (ComputerDAO) DAOFactory.getDAO("computer");
+			Computer computer_test = new Computer(574, "test", new Date(), null, company_test);
+			computerDAO.update(computer_test);
 			List<Computer> computers = computerDAO.findAll();
 			for(Computer computer : computers) {
 				System.out.println(computer);
 			}
-			*/
+			
 		} catch (FactoryException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- src/main/java/com/excilys/cdb/daos/ComputerDAO.java
+++ src/main/java/com/excilys/cdb/daos/ComputerDAO.java
@@ -15,6 +15,12 @@ public class ComputerDAO implements DAO<Computer> {
 	private Connection connection;
 	private final String FIND_ALL_COMPUTERS = "SELECT * FROM computer ORDER BY computer.id";
 	private final String FIND_ALL_MANUFACTURERS = "SELECT * FROM company, computer WHERE computer.company_id = company.id";
+	private final String FIND_COMPUTER_BY_ID = "SELECT * FROM computer WHERE id = ?";
+	private final String FIND_MANUFACTURER_BY_ID = "SELECT * FROM company WHERE id = ?";
+	private final String ADD_COMPUTER = "INSERT INTO computer (id, name) VALUES (?, ?)";
+	private final String DELETE_COMPUTER = "DELETE FROM computer WHERE id = ?";
+	private final String UPDATE_COMPUTER = "UPDATE computer SET name = ?, introduced = ?, "
+			+ "discontinued = ?, company_id= ? WHERE computer.id = ?";
 	
 	public ComputerDAO(Connection connection) {
 		this.connection = connection;
@@ -55,27 +61,85 @@ public class ComputerDAO implements DAO<Computer> {
 	}
 
 	@Override
-	public Computer findOneById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Computer findOneById(int id) throws SQLException {
+		Computer computer = null;
+		PreparedStatement statement = connection.prepareStatement(FIND_COMPUTER_BY_ID);
+		statement.setInt(1, id);
+		ResultSet rSet = statement.executeQuery();
+		int company_id = 0;
+		if(rSet.next()) {
+			computer = new Computer(rSet.getInt("id"),rSet.getString("name"),
+	    			rSet.getDate("introduced"), rSet.getDate("discontinued"), null);
+			company_id = rSet.getInt("company_id");
+		}
+		rSet.close();
+		statement.close();
+		Company company = null;
+		if(company_id != 0) {
+			PreparedStatement statement_company = connection.prepareStatement(FIND_MANUFACTURER_BY_ID);
+			statement_company.setInt(1, company_id);
+			ResultSet rSet_company = statement_company.executeQuery();
+			if(rSet_company.next()) {
+				company = new Company(company_id, rSet_company.getString("name"));
+			}
+		}
+		computer.setManufacturer(company);
+		return computer;
 	}
 
 	@Override
-	public boolean add(Computer t) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean add(Computer computer) throws SQLException {
+		PreparedStatement statement = connection.prepareStatement(ADD_COMPUTER);
+		statement.setInt(1, computer.getId());
+		statement.setString(2, computer.getName());
+		int result = statement.executeUpdate();
+		if(result == 0) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
-	public boolean delete(Computer t) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean delete(Computer computer) throws SQLException {
+		PreparedStatement statement = connection.prepareStatement(DELETE_COMPUTER);
+		statement.setInt(1, computer.getId());
+		int result = statement.executeUpdate();
+		if(result == 0) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
-	public boolean update(Computer t) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean update(Computer computer) throws SQLException {
+		PreparedStatement statement = connection.prepareStatement(UPDATE_COMPUTER);
+		java.sql.Date introducedSQL = null;
+		java.sql.Date discontinuedSQL = null;
+		
+		// Check if the dates are null or not
+		if(null != computer.getIntroduced()) {
+			introducedSQL = new java.sql.Date(computer.getIntroduced().getTime());
+		}
+		if(null != computer.getDiscontinued()) {
+			discontinuedSQL = new java.sql.Date(computer.getDiscontinued().getTime());
+		}
+		
+		// Give the statement parameters
+		statement.setString(1, computer.getName());
+		statement.setDate(2, introducedSQL);
+		statement.setDate(3, discontinuedSQL);
+		if(null == computer.getManufacturer()) {
+			statement.setObject(4, null);
+		} else {
+			statement.setObject(4, computer.getManufacturer().getId());
+		}
+		statement.setInt(5, computer.getId());
+		int result = statement.executeUpdate();
+		
+		if(result == 0) {
+			return false;
+		}
+		return true;
 	}
 
 }
