c160ce3747ab6e34243b890125a8c0c7b27fb4b9
eherrmann <eherrmann@excilys.com>
2018-06-19 16:36:07 +0200

    [FEAT] image URL size is limited to 255 character

--- core/src/main/java/com/excilys/cdb/model/Company.java
+++ core/src/main/java/com/excilys/cdb/model/Company.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 /**
  * The class describing a company.
@@ -38,6 +39,7 @@ public class Company {
 
     @Column(name = "image_url")
     @NotBlank
+    @Size(max = 255)
     private String imageUrl;
 
     /**
