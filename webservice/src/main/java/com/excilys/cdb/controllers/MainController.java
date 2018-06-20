c71579bdcc0344d9118405fa965c824bd1f190b2
De Carvalho <wdecarvalho@excilys.com>
2018-06-20 11:28:01 +0200

    [FEAT] : Request for get role of user

--- webservice/src/main/java/com/excilys/cdb/controllers/MainController.java
+++ webservice/src/main/java/com/excilys/cdb/controllers/MainController.java
@@ -1,17 +1,29 @@
 package com.excilys.cdb.controllers;
 
 import java.util.HashMap;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.excilys.cdb.services.UserService;
+
 @RestController
 @CrossOrigin(origins = "*", allowCredentials="true", allowedHeaders= {"x-auth-token","x-requested-with","x-xsrf-token","X-Requested-With","Content-Type","Access-Control-Allow-Origin"})
-public class MainController {
+public class MainController {   
+    
+    private UserService service;
+    
+    public MainController(UserService userService) {
+        this.service = userService;
+    }
     
     @GetMapping(path = "/login", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)  
     public @ResponseBody HashMap<String, String> showLogin() {
@@ -32,5 +44,11 @@ public class MainController {
         HashMap<String, String> hashMap = new HashMap<>();
         hashMap.put("Message", "You logout successfull");
         return hashMap;
+    }
+    
+    @GetMapping(path="/user/{name}/roles")
+    public Set<String> getRoles(@PathVariable("name") final String name){
+        final UserDetails userDetails = service.loadUserByUsername(name);
+        return userDetails.getAuthorities().stream().map(us -> us.getAuthority()).collect(Collectors.toSet());
     }
 }
